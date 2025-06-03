218be940a5451f2cd795861885314ab916baf32c
liverHawk <hawkyofkn5365@gmail.com>
2025-06-03 21:57:17 +0900

    well

--- gulp.d/tasks/test.js
+++ gulp.d/tasks/test.js
@@ -6,37 +6,97 @@ module.exports = (src, dest, preview) => () => {
   const postcssImport = require("postcss-import");
   const postcssVar = require("postcss-custom-properties");
   const postcssCalc = require("postcss-calc");
+  const postcssUrl = require("postcss-url");
   const browserify = require("browserify");
   const concat = require("gulp-concat");
   const uglify = require("gulp-uglify");
   const { Transform } = require("stream");
   const map = transform => new Transform({ objectMode: true, transform });
+  const through = () => map((file, enc, next) => next(null, file));
   const merge = require("merge-stream");
   const fs = require("fs-extra");
+  const ospath = require("path");
+  const path = ospath.posix;
+  const imagemin = require("gulp-imagemin");
+  const imageminMozjpeg = require("imagemin-mozjpeg");
+
+  const opts = { base: src, cwd: src };
+  const sourcemaps = preview || process.env.SOURCEMAPS === "true";
 
   const postcssPlugins = [
     postcssImport,
+    (css, { messages, opts: { file } }) =>
+      Promise.all(
+        messages
+          .reduce(
+            (accum, { file: depPath, type }) =>
+              type === "dependency" ? accum.concat(depPath) : accum,
+            []
+          )
+          .map(importedPath => fs.stat(importedPath).then(({ mtime }) => mtime))
+      ).then(mtimes => {
+        const newestMtime = mtimes.reduce(
+          (max, curr) => (!max || curr > max ? curr : max),
+          file.stat.mtime
+        );
+        if (newestMtime > file.stat.mtime)
+          file.stat.mtimeMs = +(file.stat.mtime = newestMtime);
+      }),
+    postcssUrl([
+      {
+        filter: asset =>
+          new RegExp(
+            "^[~][^/]*(?:font|typeface)[^/]*/.*/files/.+[.](?:ttf|woff2?)$"
+          ).test(asset.url),
+        url: asset => {
+          const relpath = asset.pathname.slice(1);
+          const abspath = require.resolve(relpath);
+          const basename = ospath.basename(abspath);
+          const destpath = ospath.join(dest, "font", basename);
+          if (!fs.pathExistsSync(destpath)) fs.copySync(abspath, destpath);
+          return path.join("..", "font", basename);
+        }
+      }
+    ]),
     postcssVar({ preserve: preview }),
     preview ? postcssCalc : () => {},
     autoprefixer,
-    preview ? () => {} : cssnano({ preset: "default" })
+    preview
+      ? () => {}
+      : (css, result) =>
+          cssnano({ preset: "default" })(css, result).then(() =>
+            postcssPseudoElementFixer(css, result)
+          )
   ];
 
   function bundle({ base: basedir, ext: bundleExt = ".bundle.js" }) {
     return map((file, enc, next) => {
       if (bundleExt && file.relative.endsWith(bundleExt)) {
+        const mtimePromises = [];
         const bundlePath = file.path;
         browserify(file.relative, { basedir, detectGlobals: false })
           .plugin("browser-pack-flat/plugin")
-          .bundle((bundleError, bundleBuffer) => {
-            if (bundleBuffer !== undefined) file.contents = bundleBuffer;
-            next(
-              bundleError,
-              Object.assign(file, {
-                path: file.path.slice(0, file.path.length - 10) + ".js"
-              })
-            );
-          });
+          .on("file", bundledPath => {
+            if (bundledPath !== bundlePath)
+              mtimePromises.push(fs.stat(bundledPath).then(({ mtime }) => mtime));
+          })
+          .bundle((bundleError, bundleBuffer) =>
+            Promise.all(mtimePromises).then(mtimes => {
+              const newestMtime = mtimes.reduce(
+                (max, curr) => (curr > max ? curr : max),
+                file.stat.mtime
+              );
+              if (newestMtime > file.stat.mtime)
+                file.stat.mtimeMs = +(file.stat.mtime = newestMtime);
+              if (bundleBuffer !== undefined) file.contents = bundleBuffer;
+              next(
+                bundleError,
+                Object.assign(file, {
+                  path: file.path.slice(0, file.path.length - 10) + ".js"
+                })
+              );
+            })
+          );
         return;
       }
       fs.readFile(file.path, "UTF-8").then(contents => {
@@ -45,25 +105,61 @@ module.exports = (src, dest, preview) => () => {
     });
   }
 
+  function postcssPseudoElementFixer(css, result) {
+    css.walkRules(/(?:^|[^:]):(?:before|after)/, rule => {
+      rule.selector = rule.selectors
+        .map(it => it.replace(/(^|[^:]):(before|after)$/, "$1::$2"))
+        .join(",");
+    });
+  }
+
   return merge(
+    vfs.src("ui.yml", { ...opts, allowEmpty: true }),
     vfs
-      .src(["css/site.css", "css/vendor/*.css"], { base: src, cwd: src })
-      .pipe(postcss(file => ({ plugins: postcssPlugins, options: { file } }))),
-    vfs
-      .src("js/+([0-9])-*.js", { base: src, cwd: src })
-      .pipe(bundle({ base: src }))
+      .src("js/+([0-9])-*.js", { ...opts, read: false, sourcemaps })
+      .pipe(bundle(opts))
       .pipe(uglify({ ie: true, module: false, output: { comments: /^! / } }))
       .pipe(concat("js/site.js")),
     vfs
-      .src("js/vendor/+([^.])?(.bundle).js", { base: src, cwd: src })
-      .pipe(bundle({ base: src }))
+      .src("js/vendor/+([^.])?(.bundle).js", { ...opts, read: false })
+      .pipe(bundle(opts))
       .pipe(uglify({ ie: true, module: false, output: { comments: /^! / } })),
     vfs
-      .src("js/vendor/*.min.js", { base: src, cwd: src })
+      .src("js/vendor/*.min.js", opts)
       .pipe(
         map((file, enc, next) =>
           next(null, Object.assign(file, { extname: "" }, { extname: ".js" }))
         )
-      )
-  ).pipe(vfs.dest(dest));
+      ),
+    vfs
+      .src(["css/site.css", "css/vendor/*.css"], { ...opts, sourcemaps })
+      .pipe(postcss(file => ({ plugins: postcssPlugins, options: { file } }))),
+    vfs.src("font/*.{ttf,woff*(2)}", opts),
+    vfs.src("img/**/*.{gif,ico,jpg,png,svg}", opts).pipe(
+      preview
+        ? through()
+        : imagemin(
+            [
+              imagemin.gifsicle(),
+              imageminMozjpeg(),
+              imagemin.optipng(),
+              imagemin.svgo({
+                plugins: [
+                  { cleanupIDs: { preservePrefixes: ["icon-", "view-"] } },
+                  { removeViewBox: false },
+                  { removeDesc: false }
+                ]
+              })
+            ].reduce((accum, it) => (it ? accum.concat(it) : accum), [])
+          )
+    ),
+    vfs.src("helpers/*.js", opts),
+    vfs.src("layouts/*.hbs", opts),
+    vfs.src("partials/*.hbs", opts),
+    vfs.src("static/**/*[!~]", {
+      ...opts,
+      base: ospath.join(src, "static"),
+      dot: true
+    })
+  ).pipe(vfs.dest(dest, { sourcemaps: sourcemaps && "." }));
 };
